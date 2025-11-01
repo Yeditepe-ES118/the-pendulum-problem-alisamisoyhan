1b96fd897947eeb209fce34a79124c62f8fa20c1
alisamisoyhan <alisami.soyhan@std.yeditepe.edu.tr>
2025-11-01 14:50:57 +0300

    add assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -5,9 +5,9 @@ def find_period(L0,L1):
     T1 = 2* np.pi * np.sqrt(L1 /g)
     for L in range(L0, L1 + 1):
         T_current = 2* np.pi * np.sqrt(L/ g)
-        print (f"when L= {L:.1f} m, T={T_current:.1f} s")
+        print(f"when L = {float(L):.1f} m, T = {T_current:.1f} s")
     return T0, T1  
-#myresult=find_period(2,10)  
+myresult=find_period(2,10)  
         
 
     
